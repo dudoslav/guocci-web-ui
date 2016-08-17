f8cbcf99652dcc4ecc36d73f71f331a33f9ca2ef
Dusan Baran <dbaran@hotmail.sk>
2016-08-17 13:27:06 +0200

    Added test for user service

--- app/user/user.service.spec.ts
+++ app/user/user.service.spec.ts
@@ -1,7 +1,7 @@
 import { describe, expect, beforeEach, it, inject, beforeEachProviders } from '@angular/core/testing';
-import {provide} from '@angular/core';
-import {Headers, HTTP_PROVIDERS, BaseRequestOptions, XHRBackend, Response} from '@angular/http';
-import {MockBackend} from '@angular/http/testing';
+import { provide } from '@angular/core';
+import { BaseRequestOptions, Http, Response, ResponseOptions } from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
 
 import { User } from './user';
 
@@ -9,11 +9,35 @@ import { UserService } from './user.service';
 
 describe('UserService', () => {
 
-  beforeEachProviders(() => {
-    return [ HTTP_PROVIDERS, provide(XHRBackend, {useClass: MockBackend}), UserService ];
+  let mockBackend: MockBackend;
+  let service: UserService;
+
+  beforeEachProviders(() => [
+    provide('webApiBaseUrl', { useValue: 'https://guocci-mock-server.herokuapp.com/v1' }),
+    UserService,
+    MockBackend,
+    BaseRequestOptions,
+    provide(Http, {
+      useFactory: (backend: MockBackend, options: BaseRequestOptions) => new Http(backend, options),
+      deps: [ MockBackend, BaseRequestOptions ]
+    })
+  ]);
+
+  beforeEach(inject([ MockBackend, UserService ], (_mockBackend: MockBackend, _service: UserService) => {
+    mockBackend = _mockBackend;
+    service = _service;
+  }));
+
+  it('should fetch right mocked user', () => {
+    let response = {id: 42, name: 'Chose Rodrigez', email: 'chose.rodrigez@imaginary.cloud.cz'};
+    mockBackend.connections.subscribe((connection: MockConnection) => {
+      connection.mockRespond(new Response(new ResponseOptions({ body: JSON.stringify(response)})));
+    });
+    service.getUser().subscribe(res => {
+      let user: User = res;
+      expect(user.id).toBe(42);
+      expect(user.name).toBe('Chose Rodrigez');
+      expect(user.email).toBe('chose.rodrigez@imaginary.cloud.cz');
+    });
   });
-
-  /*it('should fetch right data', inject([XHRBackend, UserService], (mockBackend, userService) => {
-
-  }));*/
 });
