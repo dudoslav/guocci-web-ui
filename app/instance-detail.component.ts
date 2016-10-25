22d3d6bcff83da575aa499882e78726ca52c1d00
Dusan Baran <dbaran@hotmail.sk>
2016-10-25 12:00:37 +0200

    Fixed bug that opened collapsible using button in header
    This one is my life achievement

--- app/instance-detail.component.ts
+++ app/instance-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, QueryList, ViewChildren, AfterViewInit, ElementRef } from '@angular/core';
 
 import { Instance } from './shared/instance';
 import { Site } from './shared/site';
@@ -7,6 +7,7 @@ import { Appliance } from './shared/appliance';
 import { Interface } from './shared/interface';
 import { GuocciService } from './shared/guocci.service';
 
+declare var jQuery: any;
 
 @Component({
   selector: '[instance-detail]',
@@ -40,7 +41,7 @@ import { GuocciService } from './shared/guocci.service';
   }
   `]
 })
-export class InstanceDetailComponent implements OnInit {
+export class InstanceDetailComponent implements OnInit, AfterViewInit {
 
   @Input()
   instanceData: [ Site, Instance ];
@@ -49,6 +50,8 @@ export class InstanceDetailComponent implements OnInit {
   interfaces: Interface[];
   @Output()
   onInstanceDelete = new EventEmitter();
+
+  @ViewChildren('dontcollapse') dontCollapseElements: QueryList<ElementRef>;
 
   constructor(private guocciService: GuocciService) {
 
@@ -62,6 +65,10 @@ export class InstanceDetailComponent implements OnInit {
     });
     this.guocciService.getInterfacesOnSiteForInstance(this.instanceData[1].id, this.instanceData[0].id).
       subscribe(res => this.interfaces = res as Interface[]);
+  }
+
+  ngAfterViewInit() {
+    this.dontCollapseElements.forEach( e => jQuery(e.nativeElement).on('click.collapse', (e:any) => e.stopPropagation()));
   }
 
   doDelete() {
